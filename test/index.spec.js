a3cd2d2c64ac2fd4ef77919e12c5744c192e38e9
Alex Ewerlöf <alex.ewerlof@schibsted.com>
2017-01-09 20:44:19 +0100

    added a test case for error that was fixed in #8

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,12 +1,16 @@
 const { expect } = require('chai');
 const micromustache = require('../');
+const compile = require('../lib/compile');
+const render = require('../lib/render');
 
 describe('micromustache', () => {
   it('exports render function', () => {
     expect(micromustache.render).to.be.a('function');
+    expect(micromustache.render).to.equal(render);
   });
 
   it('exports compile function', () => {
     expect(micromustache.compile).to.be.a('function');
+    expect(micromustache.compile).to.equal(compile);
   });
 });
