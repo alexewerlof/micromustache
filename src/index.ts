5ba1556a6b63f3019bb9a1b01d32e449f3c11809
Alex Ewerlöf <hanifbbz@gmail.com>
2019-02-03 23:25:49 +0100

    give build script a bit of love

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import { render } from './render'
+export default render
+
+export { render } from './render'
+export { compile } from './compile'
+export { tokenize, NameToken } from './tokenize'
+export { toPath } from './to-path'
+export { get, getKeys } from './get'
+export { stringify, stringifyValues } from './stringify'
+
+// tslint:disable-next-line no-default-export
