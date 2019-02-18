d93e9e409006432b062030a07c052a9e712dfba1
Alex Ewerlöf <hanifbbz@gmail.com>
2019-02-18 22:55:46 +0100

    updated tag examples

--- examples/tag-interpolate.js
+++ examples/tag-interpolate.js
@@ -1,4 +1,4 @@
-const { compileTag } = require('../lib/index')
+const { compileTag, renderTag } = require('../lib/index')
 
 const person = {
   firstName: 'Alex',
@@ -8,4 +8,8 @@ const person = {
 
 console.log(
   compileTag()`Hi! My name is ${'firstName'} ${'lastName'} and I am ${'nationality'}`.render(person)
+)
+
+console.log(
+  renderTag(person)`Hi! My name is ${'firstName'} ${'lastName'} and I am ${'nationality'}`
 )
--- examples/tag.js
+++
@@ -1,7 +0,0 @@
-const { toTemplate, toTemplateOpt } = require('../lib/index')
-
-const what = 'name'
-const yo = 'YO'
-const mo = 'MO'
-console.log(toTemplate`Hi! My name is ${what}${yo}. How are you?${mo}`)
-console.log(toTemplateOpt('<')`Hi! My name is ${what}${yo}. How are you?${mo}`)
