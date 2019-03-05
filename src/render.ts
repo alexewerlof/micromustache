8cb00860c41c5670005a25d69ecc32fa75a87c4e
Alex Ewerlöf <hanifbbz@gmail.com>
2019-03-05 18:20:20 +0100

    inlined stringify()

--- src/render.ts
+++ src/render.ts
@@ -12,53 +12,7 @@ export interface IStringifyOptions {
 
 const OBJECT_TO_STRING = Object.prototype.toString
 
-/**
- * Converts a value to a string
- * If value is an object with a toString() function, that is used to convert it to string.
- * Otherwise JSON.stringify() is used
- * @param value - the value to be converted to string
- * @param options - the options from compile()
- * @returns - the value converted to string
- */
-export function stringify(
-  value: any,
-  { invalidType = '', invalidObj = '{...}' }: IStringifyOptions = {}
-): string {
-  switch (typeof value) {
-    case 'string':
-      return value
-    case 'boolean':
-      return String(value)
-    case 'number':
-      if (value === Number.POSITIVE_INFINITY) {
-        return '∞'
-      }
-      if (value === Number.NEGATIVE_INFINITY) {
-        return '-∞'
-      }
-      // including NaN
-      return String(value)
-    case 'object':
-      // null is an object but is falsy. Swallow it.
-      if (value === null) {
-        return ''
-      }
-      if (isFunction(value.toString) && value.toString !== OBJECT_TO_STRING) {
-        return value.toString()
-      }
-      try {
-        return JSON.stringify(value)
-      } catch (jsonError) {
-        return invalidObj
-      }
-    case 'undefined':
-      return ''
-    default:
-      // Anything else will be replaced with an empty string
-      // For example: undefined, Symbol, etc.
-      return invalidType
-  }
-}
+type ConvertibleToString = string | number | boolean
 
 /**
  * The callback for resolving a value
@@ -82,7 +36,7 @@ export class Renderer {
     [path: string]: string[]
   } = {}
 
-  private assembleCache: string[]
+  private assembleCache: ConvertibleToString[]
 
   constructor(
     private tokens: ITagInput,
@@ -104,10 +58,39 @@ export class Renderer {
     }
   }
 
-  private assembleResults(values: any[]): string {
+  private assembleResults(
+    values: any[],
+    { invalidType = '', invalidObj = '{...}' }: IStringifyOptions = {}
+  ): string {
     const { length } = values
     for (let i = 0; i < length; i++) {
-      this.assembleCache[i * 2 + 1] = stringify(values[i], this.options)
+      const value = values[i]
+      const typeOfValue = typeof value
+      const valueIndex = i * 2 + 1
+      if (
+        value === null ||
+        value === undefined ||
+        typeOfValue === 'string' ||
+        typeOfValue === 'number' ||
+        typeOfValue === 'boolean'
+      ) {
+        this.assembleCache[valueIndex] = value
+      } else if (typeOfValue === 'object') {
+        // At this point in the function we've already dealt with null values
+        if (isFunction(value.toString) && value.toString !== OBJECT_TO_STRING) {
+          this.assembleCache[valueIndex] = value.toString()
+        } else {
+          try {
+            this.assembleCache[valueIndex] = JSON.stringify(value)
+          } catch (jsonError) {
+            this.assembleCache[valueIndex] = invalidObj
+          }
+        }
+      } else {
+        // Anything else will be replaced with an empty string
+        // For example: undefined, Symbol, etc.
+        this.assembleCache[valueIndex] = invalidType
+      }
     }
     return this.assembleCache.join('')
   }
