396f09c535b146a12a9d3bd0f2bca13b32b2a642
Alex Ewerlöf <hanifbbz@gmail.com>
2019-03-05 17:40:54 +0100

    improve performance tests for native

--- perf/native.js
+++ perf/native.js
@@ -18,6 +18,20 @@ function strConcat(obj) {
   )
 }
 
+function strConcatMultiline(obj) {
+  let ret = ''
+  ret += 'Hi, My name is '
+  ret += obj.name
+  ret += '! I am '
+  ret += obj.age
+  ret += ' years old and live in '
+  ret += obj.cities[1]
+  ret += '. foo is '
+  ret += obj.nested.foo
+  ret += '.'
+  return ret
+}
+
 function strJoin(obj) {
   const ret = ['Hi, My name is ']
   ret.push(obj.name)
@@ -31,8 +45,22 @@ function strJoin(obj) {
   return ret.join('')
 }
 
+function strJoinPreAllocated(obj) {
+  const ret = new Array(9)
+  ret[0] = 'Hi, My name is '
+  ret[1] = obj.name
+  ret[2] = '! I am '
+  ret[3] = obj.age
+  ret[4] = ' years old and live in '
+  ret[5] = obj.cities[1]
+  ret[6] = '. foo is '
+  ret[7] = obj.nested.foo
+  ret[8] = '.'
+  return ret.join('')
+}
+
 module.exports = {
   name: '(Native)',
   csp: true,
-  cases: [templateLiteral, strConcat, strJoin]
+  cases: [templateLiteral, strConcat, strConcatMultiline, strJoin, strJoinPreAllocated]
 }
