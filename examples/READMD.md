acfcb15bbd96d77b7bc6ec30b1528611305062b9
Alex Ewerlöf <hanifbbz@gmail.com>
2019-03-06 19:08:12 +0100

    now the examples live in their own package

---
+++ examples/READMD.md
@@ -0,0 +1,6 @@
+# Into
+
+Since the examples use some other packages that didn't make sense to add as a direct dependency to the main package, this file has its own `package.json`.
+
+So please run `npm i` to install all dependencies.
+Also make sure that micromustache is built and `../lib` is populated: `npm run build`.
--- examples/async-resolve.js
+++ examples/async-resolve.js
@@ -1,61 +1,15 @@
-const https = require('https')
+const fetch = require('node-fetch')
 const { renderFnAsync } = require('../lib/render')
-
-// Run it with `node -r ts-node/register examples/async-resolve.js`
-
-// from https://nodejs.org/api/http.html#http_http_get_url_options_callback
-function fetch(url) {
-  return new Promise((resolve, reject) => {
-    https
-      .get(url, res => {
-        const { statusCode } = res
-        const contentType = res.headers['content-type']
-
-        let error
-        if (statusCode !== 200) {
-          error = new Error('Request Failed.\n' + `Status Code: ${statusCode}`)
-        } else if (!/^application\/json/.test(contentType)) {
-          error = new Error(
-            'Invalid content-type.\n' + `Expected application/json but received ${contentType}`
-          )
-        }
-        if (error) {
-          console.error(error.message)
-          // consume response data to free up memory
-          res.resume()
-          return reject(error)
-        }
-
-        res.setEncoding('utf8')
-        let rawData = ''
-        res.on('data', chunk => {
-          rawData += chunk
-        })
-        res.on('end', () => {
-          try {
-            const parsedData = JSON.parse(rawData)
-            resolve(parsedData)
-          } catch (e) {
-            reject(e.message)
-          }
-        })
-      })
-      .on('error', e => {
-        reject(e.message)
-      })
-  })
-}
-
-/*
-https://od-api-demo.oxforddictionaries.com:443/api/v1/domains/{source_domains_language}/{target_domains_language}
-*/
-renderFnAsync(
-  'The first todo title is {{1}}',
-  {},
-  {
-    resolver: async function(taskId) {
-      const taskData = await fetch('https://jsonplaceholder.typicode.com/todos/' + taskId)
+const main = require('async-main').default
+main(async () => {
+  try {
+    const result = await renderFnAsync('The first todo title is {{1}}', async function(taskId) {
+      const response = await fetch('https://jsonplaceholder.typicode.com/todos/' + taskId)
+      const taskData = await response.json()
       return taskData.title
-    }
+    })
+    console.log(result)
+  } catch (e) {
+    console.error(e)
   }
-).then(console.log, console.error)
+})
