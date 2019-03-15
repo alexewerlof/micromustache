be49eca7f868dd634dbcf8f50e9b83c85a0c6665
Alex Ewerlöf <hanifbbz@gmail.com>
2019-03-15 21:40:30 +0100

    added an example demonstrating pipes

---
+++ examples/pipe.js
@@ -0,0 +1,33 @@
+const { renderFn, get } = require('../dist/node')
+
+function compose(fnArr, initialValue) {
+  return fnArr.reduce((input, fn) => fn(input), initialValue)
+}
+
+const pipes = {
+  charCount(str) {
+    return str.length
+  },
+  stars(n) {
+    return '*'.repeat(n)
+  }
+}
+
+const template = "{{name}}'s password is {{password | charCount | stars}}!"
+
+function resolveFn(varName, scope) {
+  const [vn, ...pipeNames] = varName.split('|')
+  const value = get(scope, vn)
+  if (pipeNames.length) {
+    const fnArr = pipeNames.map(pipeName => pipes[pipeName.trim()])
+    return compose(
+      fnArr,
+      value
+    )
+  }
+  return value
+}
+
+console.log(
+  renderFn(template, resolveFn, { name: 'Kathrina', password: 'MonkeyIsland' })
+)
