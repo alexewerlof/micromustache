de70cf3e63b33e70d88a403750e5fd85c2d5340c
Alex Ewerlöf <hanifbbz@gmail.com>
2019-03-17 14:53:43 +0100

    revided tsdocs for render

--- src/render.ts
+++ src/render.ts
@@ -3,18 +3,18 @@ import { Scope } from './get'
 import { compile, ICompileOptions } from './compile'
 
 /**
- * Replaces every {{variable}} inside the template with values provided by scope.
+ * Replaces every {{varName}} inside the template with values from the scope
+ * parameter.
  *
- * @param template - The template containing one or more {{variableNames}} every variable
- *        names that is used in the template. If it's omitted, it'll be assumed an empty object.
- * @param scope - An object containing values for every variable names that is used in
- *        the template. If it's omitted and empty object will be assumed.
- * @param renderNullAndUndefined - should we render null as 'null' and undefined as 'undefined'
+ * @param template - The template containing one or more {{variableName}} as
+ * placeholders for values from the `scope` parameter.
+ * @param scope - An object containing values for variable names from the the
+ * template. If it's omitted, we default to an empty object.
+ * If a value does not exist in the scope, two things can happen:
+ * * if `allowInvalidVarNames` is truthy, the value will be assumed empty string
+ * * if `allowInvalidVarNames` is falsy, a ReferenceError will be thrown
  * @returns - Template where its variable names replaced with
- *        corresponding values. If a value is not found or is invalid, it will
- *        be assumed empty string ''. If the value is an object itself, it'll
- *        be stringified by JSON.
- *        In case of a JSON stringify error the result will look like "{...}".
+ * corresponding values.
  */
 export function render(
   template: string,
@@ -26,7 +26,9 @@ export function render(
 }
 
 /**
- * Same as render() but calls the resolver asynchronously
+ * Same as render() but accepts a resolver function which will be responsible
+ * for returning a value for every varName.
+ * The signature of the resolver function: `(varName, scope) => string`
  */
 export function renderFn(
   template: string,
@@ -39,7 +41,9 @@ export function renderFn(
 }
 
 /**
- * Same as render() but calls the resolver asynchronously
+ * Same as renderFn() but only works with asynchronous resolver functions
+ * and returns a promise instead of the rendered promise.
+ * The signature of the resolver function: `(varName, scope) => Promise<string>`
  */
 export function renderFnAsync(
   template: string,
