953556aea73f809a538e3d7211b587185f055fa0
Alex Ewerlöf <hanifbbz@gmail.com>
2019-06-06 20:52:24 +0200

    update example dependencies

--- examples/async-resolve.js
+++ examples/async-resolve.js
@@ -1,11 +1,11 @@
 const fetch = require('node-fetch')
 const { renderFnAsync } = require('../dist/node')
-const main = require('async-main').default
-main(async () => {
+const am = require('am')
+am(async () => {
   try {
     const result = await renderFnAsync(
       'The first todo title is {{1}}',
-      async function(taskId) {
+      async function (taskId) {
         const response = await fetch(
           'https://jsonplaceholder.typicode.com/todos/' + taskId
         )
