cb760e9ffdeb20f668287277560da4232c55f56c
Alex Ewerlöf <hanifbbz@gmail.com>
2019-11-10 23:23:44 +0100

    use rollup to build

---
+++ rollup.config.js
@@ -0,0 +1,52 @@
+import { terser } from 'rollup-plugin-terser'
+import gzipPlugin from 'rollup-plugin-gzip'
+import typescript from 'rollup-plugin-typescript'
+import { name, version } from './package.json'
+
+const banner = `/* ${name} v${version} */`
+const sourcemap = true
+
+export default [
+    {
+        input: 'src/index.ts',
+        plugins: [
+            typescript({
+                target: 'es5',
+                module: 'es6',
+                lib: ['es2015'],
+            }),
+            terser({
+                include: /\.min\.js$/,
+                safari10: true,
+            }),
+            gzipPlugin()
+        ],
+        output: [
+            {
+                file: 'dist/cjs.js',
+                format: 'cjs',
+                banner,
+                sourcemap,
+            },
+            {
+                file: 'dist/umd.js',
+                name,
+                format: 'umd',
+                banner,
+                sourcemap,
+            },
+            {
+                file: 'dist/esm.js',
+                format: 'esm',
+                banner,
+                sourcemap,
+            },
+            {
+                file: 'dist/esm.min.js',
+                format: 'esm',
+                banner,
+                sourcemap,
+            },
+        ]
+    }
+]
