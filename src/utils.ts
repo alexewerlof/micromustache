f2955086fa7ee344efee2ca276c7d9c2bea8ca5f
Alex Ewerlöf <hanifbbz@gmail.com>
2020-07-14 10:27:24 +0200

    refactor: remove the extra confusing param

--- src/utils.ts
+++ src/utils.ts
@@ -14,8 +14,8 @@ export function isNum(x: unknown): x is number {
   return Number.isFinite(x as number)
 }
 
-export function isInt(x: unknown, shouldBePositiveOr0 = false): x is number {
-  return Number.isInteger(x) && (!shouldBePositiveOr0 || (x as number) >= 0)
+export function isInt(x: unknown): x is number {
+  return Number.isInteger(x)
 }
 
 export function isArr(x: unknown): x is unknown[] {
