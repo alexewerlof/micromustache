49f8b0cce25d9939c853d87484cb8649b2e5e77f
Alex Ewerlöf <hanifbbz@gmail.com>
2020-08-08 00:21:48 +0200

    improve the deep perf test

--- perf/against-mustache.js
+++ perf/against-mustache.js
@@ -11,13 +11,13 @@ const ITERATIONS = 10000
 
 const hrtime2ms = ([sec, nano]) => sec * 1e3 + nano / 1e6
 function x(a, b) {
-  const ratio = a / b
+  const ratio = b / a
   if (ratio < 1) {
     return ''
   }
 
   let ret = '🔥 ('
-  ret += ratio < 2 ? (((a - b) * 100) / b).toFixed(0) + '%' : ratio.toFixed(1) + 'x'
+  ret += ratio < 2 ? (((b - a) * 100) / a).toFixed(0) + '%' : ratio.toFixed(1) + 'x'
   ret += ' faster)'
   return ret
 }
@@ -39,8 +39,8 @@ function compare(f1, f2) {
   }
   const f2duration = hrtime2ms(process.hrtime(start))
 
-  console.log(`${f1name}: ${ms(f1duration)} ${x(f2duration, f1duration)}`)
-  console.log(`${f2name}: ${ms(f2duration)} ${x(f1duration, f2duration)}`)
+  console.log(`${f1name}: ${ms(f1duration)} ${x(f1duration, f2duration)}`)
+  console.log(`${f2name}: ${ms(f2duration)} ${x(f2duration, f1duration)}`)
 }
 
 const LEN = 1000
@@ -48,23 +48,6 @@ const LEN = 1000
 const longVarName = 'x'.repeat(LEN)
 const longScope = { [longVarName]: 'y'.repeat(LEN) }
 const longTemplate = '{{' + longVarName + '}} and '.repeat(LEN)
-
-const diverseScope = {}
-for (let i = 0; i < LEN; i++) {
-  diverseScope[`key_${i}`] = i + ' is ' + Math.random()
-}
-const diverseTemplate = Object.keys(diverseScope)
-  .map((key) => `{{${key}}}`)
-  .join(' ')
-
-const deepScope = {}
-const indices = []
-for (let i = 0, currObj = deepScope; i < LEN; i++) {
-  currObj[i] = {}
-  currObj = currObj[i]
-  indices.push(i)
-}
-const deepTemplate = '{{' + indices.join('.') + '}}'
 
 function micromustacheLong() {
   return micromustache.render(longTemplate, longScope, { maxVarNameLength: 999999 })
@@ -74,6 +57,14 @@ function mustacheLong() {
   return mustache.render(longTemplate, longScope)
 }
 
+const diverseScope = {}
+for (let i = 0; i < LEN; i++) {
+  diverseScope[`key_${i}`] = i + ' is ' + Math.random()
+}
+const diverseTemplate = Object.keys(diverseScope)
+  .map((key) => `{{${key}}}`)
+  .join(' ')
+
 function micromustacheDiverse() {
   return micromustache.render(diverseTemplate, diverseScope)
 }
@@ -81,6 +72,18 @@ function micromustacheDiverse() {
 function mustacheDiverse() {
   return mustache.render(diverseTemplate, diverseScope)
 }
+
+const deepScope = {}
+const indices = []
+for (let i = 0, currObj = deepScope; i < LEN; i++) {
+  // Generate random string like '4ea3f82979'
+  const index = Math.random().toString(16).substring(2, 12)
+  const newObj = {}
+  currObj[index] = newObj
+  currObj = newObj
+  indices.push(index)
+}
+const deepTemplate = '{{' + indices.join('.') + '}}'
 
 function micromustacheDeep() {
   return micromustache.render(deepTemplate, deepScope, { depth: LEN, maxVarNameLength: 999999 })
